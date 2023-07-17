79ad8635439ba626dd3d7d10f4082336e3ac5db1
Brunopjunq <bruno_junq@poli.ufrj.br>
2023-07-17 16:32:04 -0300

    feat: add icons HomePage layout

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,42 +2,68 @@ import { styled } from "styled-components";
 import Header from "../components/Header";
 import Cow from "../assets/White-cow.png";
 import SideBar from "../components/SideBar";
+import { CgAdd } from 'react-icons/cg';
+import { BsSearch } from 'react-icons/bs';
 
 export default function HomePage() {
+    const styleA = { color: 'white', fontSize: '30px', position: 'absolute', top: '50%', left: '55%'};
+    const styleS = { color: 'white', fontSize: '27px', position: 'absolute', top: '52%', left: '55%'};
+
     return (
         <>
             <Header />
             <App>
                 <BoxContent color={'#592323'}>
                     <img src={Cow} />
+                    <CgAdd
+                        style={styleA}
+                    />
                     <p>Adicionar Animal</p>
                 </BoxContent>
                 <BoxContent color={'#25272b'}>
                     <img src={Cow} />
+                    <CgAdd
+                        style={styleA}
+                    />
                     <p>Adicionar Nascimento</p>
                 </BoxContent>
                 <BoxContent color={'#2b1847'}>
                     <img src={Cow} />
+                    <BsSearch
+                        style={styleS}
+                    />
                     <p>Consultar Pasto</p>
                 </BoxContent>
                 <BoxContent color={'#461a4f'}>
                     <img src={Cow} />
+                    <BsSearch
+                        style={styleS}
+                    />
                     <p>Consultar Cobertura</p>
                 </BoxContent>
                 <BoxContent color={'#4f3e16'}>
                     <img src={Cow} />
+                    <BsSearch
+                        style={styleS}
+                    />
                     <p>Consultar Animais</p>
                 </BoxContent>
                 <BoxContent color={'#0f3822'}>
                     <img src={Cow} />
+                    <CgAdd
+                        style={styleA}
+                    />
                     <p>Lançar Venda de Animal</p>
                 </BoxContent>
                 <BoxContent color={'#3b3d11'}>
                     <img src={Cow} />
+                    <CgAdd
+                        style={styleA}
+                    />
                     <p>Lançar Morte de Animal</p>
                 </BoxContent>
             </App>
-            <SideBar />
+            {/* <SideBar /> */}
         </>
     )
 }
@@ -58,6 +84,7 @@ const BoxContent = styled.div`
     flex-direction: column;
     box-sizing: border-box;
     padding: 15px;
+    position: relative;
 
     p {
         color: white;
